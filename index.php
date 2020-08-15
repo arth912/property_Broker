f874430333aa6221c1f8fefc004cd62951a79b5c
Arth Rathod <69592558+arth912@users.noreply.github.com>
2020-08-15 16:40:33 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
   <link rel="stylesheet" types="text/css" href="css/style.css">
 </head>
 <body>
-  <form method="post" action="connect.php">
   <?php
   ?>
   <header>
@@ -15,32 +14,34 @@
       </div>
       <ul>
         <li class="active"><a href="#">Home</a></li>
+        <li><a href="signup12.php">Sign Up</a></li>
         <li><a href="#">Contact Us</a></li>
         <li><a href="#">About US</a></li>
         <li><a href="#">Review</a></li>
       </ul>
     </div>
     <div class="title">
-      <h1>Broker Public Portal</h1>
-      <hr>
+      <h1>Broker Public Portal</h1><hr>
     </div>
     <fieldest><legend></legend>
-    <div class="button">
-      <h4><label for="Email">Email  Id :-</label>
-      <input type="text" name="Email Id" value="" size="30" required></h4>
-      <h4><label for="password">password:-</label>
-      <input type="password" name="Password" value="" size="30" required></h4>
-      <div class="b"><br></br>
-        </br>
-      </div>
-      <a href="signin.html" class="btn">Sign In</a>
-      <a href="signup12.php" class="btn">Sign Up</a>
-      </br>
-    </div>
-  </fieldset>
+    <form method="post" action="signin.php">
+        <div class="button">
+            <div class="btn2">
+                <h4><label for="Email">Email  Id :-</label>
+                <input type="text" name="mail" value="" size="30" required></h4>
+                <h4><label for="password">password:-</label>
+                <input type="password" name="pass" value="" size="30" required></h4>
+            </div>
+            <div class="b"><br></br>
+            </div>
+            <div class="btn1">
+                <input type="submit" value="sign in" style="padding: 5px 10px;" style="margin-left: 50%" />
+            </div>
+        </div>
+      </form>
+      </fieldset>
   <div class="problem"><p>*If You Facing Problem In Sign In Then Contact Us.</p>
   </div>
   </header>
-</form>
 </body>
 </html>
